c713acf697c3b58e99012c2f355662ed27adc9fd
Luis Fernando <luis.silva9902@gmail.com>
2024-02-24 07:42:09 -0300

    renaming variables from Portuguese to English

--- ignite-clean-code-desafios/desafios/04-codigo-em-ingles.tsx
+++ ignite-clean-code-desafios/desafios/04-codigo-em-ingles.tsx
@@ -1,5 +1,5 @@
 // Código em inglês 
-import { useState } from "react"
+import { useState } from "react";
 
 interface Product {
   title: string;
@@ -17,23 +17,23 @@ const productList = [
   }
 ]
 
-export function ListProduto() {
-  const [filteredProdutos, setFilteredProdutos] = useState<Product[]>([])
+export function productListComponent() {
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>([])
 
-  function searchProduto(search: string) {
-    const filtrado = productList.filter(product => product.title.includes(search))
+  function searchProduct(search: string) {
+    const filteredProduct = productList.filter(product => product.title.includes(search))
 
-    setFilteredProdutos(filtrado)
+    setFilteredProducts(filteredProduct)
   }
 
   return (
     <div>
-      <input type="text" onChange={(e) => searchProduto(e.target.value)} />
+      <input type="text" onChange={(e) => searchProduct(e.target.value)} />
 
-      {filteredProdutos.map(produto => (
+      {filteredProducts.map(product => (
         <div>
-          <p>{produto.title}</p>
-          <p>{produto.price}</p>
+          <p>{product.title}</p>
+          <p>{product.price}</p>
         </div>
       ))}
     </div>
