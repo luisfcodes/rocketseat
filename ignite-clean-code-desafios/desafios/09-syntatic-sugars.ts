ba6e55340f0b35d9d21bee95771f1bc61c1a1b6c
Luis Fernando <luis.silva9902@gmail.com>
2024-02-24 08:46:52 -0300

    remove syntatic sugars

--- ignite-clean-code-desafios/desafios/09-syntatic-sugars.ts
+++ ignite-clean-code-desafios/desafios/09-syntatic-sugars.ts
@@ -1,11 +1,13 @@
 function getFirstFiveRatings(ratings) {
-  return ratings.length >= 5 && ratings.slice(0, 5)
+  if(ratings.length < 5) return false
+
+  return ratings.slice(0, 5)
 }
 
 function sumFirstFiveRatings(ratings) {
-  const ratingsBool = !!ratings
+  const areThereRatings = Boolean(ratings)
 
-  if (ratingsBool) {
+  if (areThereRatings) {
     const firstFiveRatings = getFirstFiveRatings(ratings)
 
     if (!firstFiveRatings) return { error: 'there must be at least 5 ratings' }
@@ -13,10 +15,10 @@ function sumFirstFiveRatings(ratings) {
     let ratingsSum = 0;
 
     for (const rating of firstFiveRatings) {
-      ratingsSum += +rating
+      ratingsSum += Number(rating)
     }
 
-    return { ratingsSum, created_at: +new Date() }
+    return { ratingsSum, created_at: Number(new Date())}
   }
 
   return { error: 'ratings is required' }
