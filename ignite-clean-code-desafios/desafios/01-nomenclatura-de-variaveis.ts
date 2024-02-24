11535fde5020844807254d8de168e29711444fc0
Luis Fernando <luis.silva9902@gmail.com>
2024-02-24 07:17:16 -0300

    renaming variables

--- ignite-clean-code-desafios/desafios/01-nomenclatura-de-variaveis.ts
+++ ignite-clean-code-desafios/desafios/01-nomenclatura-de-variaveis.ts
@@ -1,6 +1,6 @@
 // Nomenclatura de variáveis
 
-const list = [
+const userCategory = [
   {
     title: 'User',
     followers: 5
@@ -19,37 +19,37 @@ const list = [
   },
 ]
 
-export default async function getData(req, res) {
-  const github = String(req.query.username)
+export default async function getGithubUserCategory(req, res) {
+  const githubUser = String(req.query.username)
 
-  if (!github) {
+  if (!githubUser) {
     return res.status(400).json({
       message: `Please provide an username to search on the github API`
     })
   }
 
-  const response = await fetch(`https://api.github.com/users/${github}`);
+  const githubUserSearchResult = await fetch(`https://api.github.com/users/${githubUser}`);
 
-  if (response.status === 404) {
+  if (githubUserSearchResult.status === 404) {
     return res.status(400).json({
-      message: `User with username "${github}" not found`
+      message: `User with username "${githubUser}" not found`
     })
   }
 
-  const data = await response.json()
+  const userData = await githubUserSearchResult.json()
 
-  const orderList = list.sort((a, b) =>  b.followers - a.followers); 
+  const userCategorySortedByFollowers = userCategory.sort((a, b) =>  b.followers - a.followers);
 
-  const category = orderList.find(i => data.followers > i.followers)
+  const filteredUserCategory = userCategorySortedByFollowers.find(i => userData.followers > i.followers)
 
   const result = {
-    github,
-    category: category.title
+    githubUser,
+    category: filteredUserCategory.title
   }
 
   return result
 }
 
-getData({ query: {
+getGithubUserCategory({ query: {
   username: 'josepholiveira'
 }}, {})
