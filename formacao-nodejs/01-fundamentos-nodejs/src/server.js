1f66d02dc97d5f9767c0628417ed2d754108a821
Luis Fernando <luis.silva9902@gmail.com>
2024-03-30 07:50:06 -0300

    feat: add status code

--- formacao-nodejs/01-fundamentos-nodejs/src/server.js
+++ formacao-nodejs/01-fundamentos-nodejs/src/server.js
@@ -1,7 +1,27 @@
 import http from 'node:http';
 
+const users = []
+
 const server = http.createServer((req, res) => {
-  return res.end('Hello World')
+  const { url, method } = req;
+
+  if(url === '/users' && method === 'GET') {
+    return res
+    .setHeader('Content-Type', 'application/json')
+    .end(JSON.stringify(users))
+  }
+
+  if(url === '/users' && method === 'POST') {
+    users.push({
+      id: Math.random(),
+      name: 'Lucas',
+      email: 'lucas@gmail.com'
+    })
+
+    return res.writeHead(201).end()
+  }
+
+  return res.writeHead(404).end()
 })
 
 server.listen(3333)
