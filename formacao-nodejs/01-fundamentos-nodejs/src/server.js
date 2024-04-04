9e4d80cdc5e934eb9364a62a33f78aadd4489356
Luis Fernando <luis.silva9902@gmail.com>
2024-04-04 07:09:55 -0300

    feat: create regex to catch the user id

--- formacao-nodejs/01-fundamentos-nodejs/src/server.js
+++ formacao-nodejs/01-fundamentos-nodejs/src/server.js
@@ -1,35 +1,16 @@
-import { randomUUID } from 'node:crypto';
 import http from 'node:http';
-import { Database } from './database.js';
 import { json } from './middlewares/json.js';
-
-const database = new Database()
+import { routes } from './routes.js';
 
 const server = http.createServer(async (req, res) => {
   const { url, method } = req;
 
   await json(req, res)
 
-  if(url === '/users' && method === 'GET') {
-    const users = database.select('users')
+  const route = routes.find(route => route.method === method && route.path === url)
 
-    return res
-    .setHeader('Content-Type', 'application/json')
-    .end(JSON.stringify(users))
-  }
-
-  if(url === '/users' && method === 'POST') {
-    const { name, email } = req.body
-
-    const user = ({
-      id: randomUUID(),
-      name,
-      email
-    })
-
-    database.insert('users', user)
-
-    return res.writeHead(201).end()
+  if(route){
+    return route.handler(req, res)
   }
 
   return res.writeHead(404).end()
