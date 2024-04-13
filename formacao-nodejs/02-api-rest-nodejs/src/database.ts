13f9f5ad2a32f5c58d9c2a5a21b6af5261e9b49a
Luis Fernando <luis.silva9902@gmail.com>
2024-04-13 15:05:29 -0300

    feat: add validation in env variables with zod library

--- formacao-nodejs/02-api-rest-nodejs/src/database.ts
+++ formacao-nodejs/02-api-rest-nodejs/src/database.ts
@@ -1,9 +1,10 @@
 import { Knex, knex as KnexSetup } from 'knex'
+import { env } from './env'
 
 export const config = {
   client: 'sqlite',
   connection: {
-    filename: './db/app.db',
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
--- formacao-nodejs/02-api-rest-nodejs/src/server.ts
+++ formacao-nodejs/02-api-rest-nodejs/src/server.ts
@@ -1,17 +1,20 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const test = await knex('sqlite_schema').select('*')
+  const transactions = await knex('transactions')
+    .select()
+    .where('amount', '>', 100)
 
-  return test
+  return transactions
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
     host: 'localhost',
   })
   .then(() => {
