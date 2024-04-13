4f58570dc588935c46d31bc2bfdd1ad25d858628
Luis Fernando <luis.silva9902@gmail.com>
2024-04-13 17:00:45 -0300

    feat: add methods to get all transactions and get summary amount

--- formacao-nodejs/02-api-rest-nodejs/src/routes/transactions.ts
+++ formacao-nodejs/02-api-rest-nodejs/src/routes/transactions.ts
@@ -4,6 +4,49 @@ import { z } from 'zod'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async (req, res) => {
+    const transactions = await knex('transactions').select('*')
+
+    return res.send({
+      totalTransactions: transactions.length,
+      transactions,
+    })
+  })
+
+  app.get('/:id', async (req, res) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(req.params)
+
+    const transaction = await knex('transactions').where({ id }).first()
+
+    if (!transaction) {
+      return res.status(404).send({
+        message: 'Transaction not found',
+      })
+    }
+
+    return res.send(transaction)
+  })
+
+  app.get('/summary', async (req, res) => {
+    const summary = await knex('transactions')
+      .sum('amount', {
+        as: 'amount',
+      })
+      .first()
+
+    if (!summary) {
+      return res.send({
+        amount: 0,
+      })
+    }
+
+    return res.send(summary)
+  })
+
   app.post('/', async (req, res) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
@@ -20,11 +63,5 @@ export async function transactionsRoutes(app: FastifyInstance) {
     })
 
     return res.status(201).send()
-  })
-
-  app.get('/', async (req, res) => {
-    const transactions = await knex('transactions').select('*')
-
-    return res.send(transactions)
   })
 }
