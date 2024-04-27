3285855b5f4ff0ea004f2bb85e475ad248b832fc
Luis Fernando <luis.silva9902@gmail.com>
2024-04-27 15:58:30 -0300

    feat: created project

---
+++ formacao-java/src/main/java/EstruturaDeDados.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class EstruturaDeDados {
+  public static void main(String[] args) {
+    // Lista
+    List<String> nomes = new ArrayList<>();
+    nomes.add("Luis Fernando");
+    nomes.add("Fabiana");
+
+//  System.out.println(nomes.get(0));
+
+//    for (int i = 0; i < nomes.size(); i++) {
+//      System.out.println(nomes.get(i));
+//    }
+
+//    for(String nome : nomes) {
+//      System.out.println("Nome é: " + nome);
+//    }
+
+//    nomes.forEach(nome -> System.out.println(nome));
+    nomes.forEach(System.out::println);
+  }
+}
---
+++ formacao-java/src/main/java/PrimeiraClasse.java
@@ -0,0 +1,28 @@
+public class PrimeiraClasse {
+  public static void main(String[] args) {
+    int dadoDoTipoInt = 10;
+    double dadoDoTipoDouble = 10.3;
+    float dadoDoTipoFloat = 10.43f;
+    long dadoDoTipoLong = 10333333L;
+    String dadoDoTipoString = "10";
+    boolean dadoDoTipoBoolean = false;
+
+    if (dadoDoTipoInt == 10) {
+      System.out.println("Entrou no if");
+    } else if(dadoDoTipoInt == 20) {
+      System.out.println("Entrou no if pois é 20");
+    } else {
+      System.out.println("Entrou no else");
+    }
+
+    int value = 0;
+    while (value < 3) {
+      System.out.println("While " + value);
+      value++;
+    }
+
+    for (int i = 0; i < 4; i++) {
+      System.out.println("For " + i);
+    }
+  }
+}
