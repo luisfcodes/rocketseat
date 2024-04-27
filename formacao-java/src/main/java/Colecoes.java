ba4dd7c3d41b86f1297e95ddc121d552c2dcee0e
Luis Fernando <luis.silva9902@gmail.com>
2024-04-27 16:13:45 -0300

    feat: create collections

---
+++ formacao-java/src/main/java/Colecoes.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Colecoes {
+  public static void main(String[] args) {
+    Map<String, Integer> notas = new HashMap<>();
+    notas.put("Paulo", 68);
+    notas.put("Vini", 100);
+    notas.put("Aline", 89);
+
+//    var nota = notas.get("Paulo");
+//    System.out.println(nota);
+
+    for(Map.Entry<String, Integer> entry : notas.entrySet()) {
+      String key = entry.getKey();
+      Integer value = entry.getValue();
+
+      System.out.println(key + " => " + value);
+    }
+  }
+}
