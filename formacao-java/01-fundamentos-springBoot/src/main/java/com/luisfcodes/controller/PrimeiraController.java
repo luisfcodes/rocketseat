d25deec3f1a9500d7053b78c4aad47d0e8ec5d91
Luis Fernando <luis.silva9902@gmail.com>
2024-04-28 16:41:18 -0300

    feat: working with path, body and headers params

--- formacao-java/01-fundamentos-springBoot/src/main/java/com/luisfcodes/controller/PrimeiraController.java
+++ formacao-java/01-fundamentos-springBoot/src/main/java/com/luisfcodes/controller/PrimeiraController.java
@@ -1,16 +1,43 @@
 package com.luisfcodes.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.websocket.server.PathParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/primeiraController")
 public class PrimeiraController {
 
-  @GetMapping("/primeiroMetodo")
-  public String primeiroMetodo(){
-    return "Meu primeiro método api rest";
+  @GetMapping("/primeiroMetodo/{id}")
+  public String primeiroMetodo(@PathVariable String id) {
+    return "O parâmetro é: " + id;
   }
 
+  @GetMapping("/metodoComQueryParams")
+  public String metodoComQueryParams(@RequestParam String id) {
+    return "O método metodoComQueryParams recebeu como parâmetro: " + id;
+  }
+
+  @GetMapping("/metodoComDiversosQueryParams")
+  public String metodoComDiversosQueryParams(@RequestParam Map<String, String> allParams) {
+    return "O método metodoComQueryParams recebeu como parâmetro: " + allParams.entrySet();
+  }
+
+  @PostMapping("/metodoComBodyParams")
+  public String metodoComBodyParams(@RequestBody Usuario usuario) {
+    return "metodoComBodyParams\n" + usuario.username();
+  }
+
+  @PostMapping("/metodoComHeadersParams")
+  public String metodoComHeadersParams(@RequestHeader String username) {
+    return "metodoComHeadersParams\n" + username;
+  }
+
+  @PostMapping("/metodoComVariosHeadersParams")
+  public String metodoComVariosHeadersParams(@RequestHeader Map<String, String> allParams) {
+    return "metodoComVariosHeadersParams\n" + allParams.entrySet();
+  }
+
+  record Usuario(String username){}
 }
