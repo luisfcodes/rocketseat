16bdb29abb3ea8377df64707db3aa2012066560a
Luis Fernando <luis.silva9902@gmail.com>
2024-05-04 00:47:58 -0300

    feat: create candidate controller and entity

---
+++ formacao-java/02-gestao-vagas/src/main/java/com/luisfcodes/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -0,0 +1,25 @@
+package com.luisfcodes.gestao_vagas.modules.candidate;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.UUID;
+
+@Data
+public class CandidateEntity {
+  private UUID id;
+  private String name;
+
+  @Pattern(regexp = "^(?!\\s*$).+", message = "Nome de usuário inválido, não deve conter espaço")
+  private String username;
+
+  @Email(message = "Insira um e-mail válido")
+  private String email;
+
+  @Length(min = 10, max = 100, message = "A senha deve conter de 10 a 100 caracteres")
+  private String password;
+  private String description;
+  private String curriculum;
+}
--- formacao-java/02-gestao-vagas/src/main/java/com/luisfcodes/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ formacao-java/02-gestao-vagas/src/main/java/com/luisfcodes/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,4 +1,20 @@
 package com.luisfcodes.gestao_vagas.modules.candidate.controllers;
 
+import com.luisfcodes.gestao_vagas.modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidate")
 public class CandidateController {
+
+  @PostMapping("/")
+  public void create(@Valid @RequestBody CandidateEntity candidateEntity){
+    System.out.println(candidateEntity.getEmail());
+  }
+
 }
