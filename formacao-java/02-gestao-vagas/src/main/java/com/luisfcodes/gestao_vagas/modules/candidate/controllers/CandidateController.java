cbca33decc1c34d5da4781227e6487b10b01a341
Luis Fernando <luis.silva9902@gmail.com>
2024-05-16 20:36:10 -0300

    feat: add validate user and create useCase

--- formacao-java/02-gestao-vagas/src/main/java/com/luisfcodes/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ formacao-java/02-gestao-vagas/src/main/java/com/luisfcodes/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,10 +1,10 @@
 package com.luisfcodes.gestao_vagas.modules.candidate.controllers;
 
 import com.luisfcodes.gestao_vagas.modules.candidate.CandidateEntity;
-import com.luisfcodes.gestao_vagas.modules.candidate.CandidateRepository;
+import com.luisfcodes.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class CandidateController {
 
   @Autowired
-  private CandidateRepository candidateRepository;
+  private CreateCandidateUseCase createCandidateUseCase;
 
   @PostMapping("/")
-  public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity){
-    return this.candidateRepository.save(candidateEntity);
+  public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity){
+    try {
+      var result = this.createCandidateUseCase.execute(candidateEntity);
+      return ResponseEntity.ok().body(result);
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
   }
-
 }
